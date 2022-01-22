29783ac29fd09899a9be1176635a986321304f4f
unknown <johanesanyavo@gmail.com>
2022-01-22 21:18:36 +0400

    add id on logout button

--- Automated_testing/login/welcome.php
+++ Automated_testing/login/welcome.php
@@ -3,7 +3,7 @@
 
     if(isset($_SESSION['sess_id']) && $_SESSION['sess_id'] != "") {
         echo '<h1>Welcome '.$_SESSION['sess_username'].'</h1>';
-        echo '<h4><a href="logout.php">Logout</a></h4>';
+        echo '<h4><a id="logout" href="logout.php">Logout</a></h4>';
     } 
     
     else { 
